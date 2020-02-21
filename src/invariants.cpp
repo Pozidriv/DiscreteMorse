e88649faf40547091221d3b210b7680cc88d0a14
Pozidriv <youri.tamitegama@mail.mcgill.ca>
2020-02-21 15:09:47 -0500

    Fixed some things. More bugs waiting

--- src/invariants.cpp
+++ src/invariants.cpp
@@ -6,16 +6,27 @@
 using namespace std;
 
 int count_triangles(vector<Node<int>> G) {
-   int count=0;
+   debug("count_triangles", "Yo mama");
+   int count=0, triples=0;
    for(int i=0; i<G.size(); i++) {
+      debug("count_triangles", i);
       Node<int> a = G[i];
+      int vertexc=0;
       for(int j=0; j<a.edge_ptrs.size(); j++) {
          Node<int> b = G[a.edge_ptrs[j]];
          for(int k=0; k<b.edge_ptrs.size(); k++) {
-            if(i == b.edge_ptrs[k]) {
-               count++;
-            }
+            triples++;
+            Node<int> c = G[b.edge_ptrs[k]];
+            for(int l=0; l<c.edge_ptrs.size(); l++) {
+               if(i == c.edge_ptrs[l]) {
+                  count++;
+                  vertexc++;
+               }
+            }  // END l loop
          }  // END k loop
       }  // END j loop
+      cout << " " << vertexc << " ";
    }  // END i loop
+   debug("count_triangles", "Triples encountered:", triples);
+   return count;
 }
